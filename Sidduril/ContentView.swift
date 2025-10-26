f9d420bb7afc245c3951494a25d8a0927285d07b
Yossi Eisenbach <ye9380@intl.att.com>
2025-10-26 17:17:07 +0200

    Initial Commit

---
+++ Sidduril/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sidduril
+//
+//  Created by Eisenbach, Yossi on 26/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sidduril/SiddurilApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiddurilApp.swift
+//  Sidduril
+//
+//  Created by Eisenbach, Yossi on 26/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SiddurilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
